65339e1cfb0ec50338550be08698e8a5465e29ca
reicdm <reicdmagpantay@gmail.com>
2026-04-14 11:44:26 +0800

    setup mvc

---
+++ A4-AppDev/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace A4_AppDev.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
